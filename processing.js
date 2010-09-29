1406133117ceefedae9e3d65dec2bba7ca1adfa4
Greg Borenstein <greg@mfdz.com>
2010-09-29 19:41:54 -0400

    track rotation to mouse

--- processing.js
+++ processing.js
@@ -4,10 +4,15 @@ void setup()
 	smooth();
 	fill(255, 0, 0);
 	noStroke();
+	$("#videotag")[0].pause();
+	
 }
 
 void draw() 
 {	
-	
-	ellipse(mouseX, mouseY, 20, 20);
+  if(videoIsReady()){
+    targetTime = map(mouseX, 0, screen.width, 0, $("#videotag")[0].duration);
+    goToTime(targetTime);
+  }
+
 }
